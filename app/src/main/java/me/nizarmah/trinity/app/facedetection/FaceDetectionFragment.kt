6bcbafdb7f918a8d865a2d7f00092a2052710713
Nizar Mahmoud <nizarmah@hotmail.com>
2019-09-27 12:49:43 +0300

    Feature: CameraView Preview of Camera

---
+++ app/src/main/java/me/nizarmah/trinity/app/facedetection/FaceDetectionFragment.kt
@@ -0,0 +1,56 @@
+package me.nizarmah.trinity.app.facedetection
+
+import android.os.Bundle
+import android.view.*
+import androidx.camera.core.CameraX
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.LifecycleOwner
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions
+import kotlinx.android.synthetic.main.fragment_facedetection.*
+import me.nizarmah.trinity.R
+
+class FaceDetectionFragment : Fragment(), LifecycleOwner {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_facedetection, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initViews(view)
+
+        setupViews()
+    }
+
+    private fun initViews(view: View) {
+
+    }
+
+    private fun setupViews() {
+        setupCameraView()
+    }
+
+    private fun setupCameraView() {
+        view_camera.apply {
+            bindToLifecycle(this@FaceDetectionFragment)
+
+            cameraLensFacing = CameraX.LensFacing.BACK
+
+            keepScreenOn = true
+            enableTorch(false)
+            isPinchToZoomEnabled = true
+            touchscreenBlocksFocus = false
+        }
+    }
+
+    private fun setupFaceDetection() {
+        val detectorOptions = FirebaseVisionFaceDetectorOptions.Builder()
+            .enableTracking()
+            .build()
+    }
+}
---
+++ app/src/main/java/me/nizarmah/trinity/app/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package me.nizarmah.trinity.app.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import me.nizarmah.trinity.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+}
